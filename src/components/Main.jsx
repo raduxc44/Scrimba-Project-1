f8e63b14bae0a9774005665c5cf9ea7132833f19
raduxc44 <raduxc44@gmail.com>
2022-07-02 20:14:22 +0300

    Added the cropped gray React logo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
+import logo from '../images/react-icon-gray.png'
+
 function Main() {
     return(
         <div className="main-content">
             <h1 className="main-title">Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+            <div className='list-container'>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on Github</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+                <img src={logo} alt="React logo Grayed out" className='gray-logo'/>
+            </div>
         </div>
     )
 }
